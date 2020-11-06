510c69939ffacd65ef6319a2d5a284ee05d9245c
Emilie Ågren <emilie.agren@iths.se>
2020-11-06 11:37:05 +0100

    Added landing page and some css code in style.css

---
+++ landing-pg.js
@@ -0,0 +1,47 @@
+const cardElement = document.querySelector('.random-beer-card');
+const randomBeerBtn = document.querySelector('.random-beer-btn');
+
+const url = "https://api.punkapi.com/v2/beers/random";
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function moreInfoOnclick(evt) {
+
+    const id=evt.target.getAttribute('name')
+    console.log(id)
+    const url=`./myview.html?name=${id}`
+    document.location.href=url;
+
+}
+
+
+const getRandomBeer = async() => {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+
+    removeAllChildNodes(cardElement);
+    
+    const beerImg = document.createElement('img');
+    const beerName = document.createElement('h2');
+    const infoLink = document.createElement('p');
+
+    beerImg.src = data[0].image_url;
+    if(data[0].image_url == null){
+        beerImg.src = 'punk_ipa.png';
+    }
+    beerName.textContent = data[0].name;
+    infoLink.innerHTML = 'See more';
+    
+    cardElement.appendChild(beerImg);
+    cardElement.appendChild(beerName);
+    cardElement.appendChild(infoLink);
+    infoLink.addEventListener('click', moreInfoOnclick);
+}
+
+getRandomBeer(url);
+randomBeerBtn.addEventListener('click', getRandomBeer);
